306a2b43f218c59f43e894675bd157dd01ab07d3
Nacros90 <91343291+Nacros90@users.noreply.github.com>
2021-12-01 18:33:08 +0100

    Modifications diverses (pour les suites seules)
    Correction de la syntaxe pour "U0" et "q" et ajouts de lignes vides pour une meilleure lisibilité.

--- Suite seules/Suite géométrique.py
+++ Suite seules/Suite géométrique.py
@@ -1,8 +1,8 @@
 print("Ce programme sert à générer une suite géométrique fini avec une valeur U0 et un raison q défini par l'utilisateur.")
 print()
-U=int(input("Valeur de U0 : "))
+U=int(input("Valeur de \"U0\" : "))
 U0=U
-q=int(input("Valeur de la raison q : "))
+q=int(input("Valeur de la raison \"q\" : "))
 L=int(input("Longueur de la suite : "))
 P=0
 print("U0=",U0)
@@ -11,4 +11,6 @@ for k in range (1,L):
     P=P+1
     print("U",P,"=",U)
     print("Le pas est égale à",k)
+    print()
+print()
 print("Fin de la suite.")

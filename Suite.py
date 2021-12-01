1b9eb14fb0e2bd27cbd169f7337987b9cc57fed4
Nacros90 <91343291+Nacros90@users.noreply.github.com>
2021-12-01 17:53:31 +0100

    Modification diverses
    Modification de la phrase de présentation (correction de fautes d'orthographes, correction d'erreur de syntaxe et amélioration de la lisibilité), modification de la syntaxe pour certaines lignes concernant "U0" et "q".

--- Suite.py
+++ Suite.py
@@ -1,10 +1,10 @@
-print("Ce programme sert à générer une suite soit arithmétique, soit géométriquue, avec un nombre de pas définissable, une valeur U0 définissable et une raison q définissable. Toutes les valeurs définissable sont définie par l'utilisateur ")
-Suite=int(input("Quel type de suite voulez générer (1. arithmétique; 2. géométrique)? "))
+print("Ce programme sert à générer une suite, soit arithmétique, soit géométrique, avec un nombre de pas définissable, une valeur \"U0\" définissable et une raison \"q\" définissable. Toutes les valeurs définissables sont définies par l'utilisateur ")
+Suite=int(input("Quel type de suite voulez générer (1. arithmétique; 2. géométrique) (Veuillez entrez le chiffre corrrepondant à la réponse) ? "))
 if Suite==1:
     print()
-    U=int(input("Valeur de U0 : "))
+    U=int(input("Valeur de \"U0\" : "))
     U0=U
-    q=int(input("Valeur de la raison q : "))
+    q=int(input("Valeur de la raison \"q\" : "))
     L=int(input("Nombre de pas : "))
     P=0
     print("U0=",U0)
@@ -16,9 +16,9 @@ if Suite==1:
         print()
 if Suite==2:
     print()
-    U=int(input("Valeur de U0 : "))
+    U=int(input("Valeur de \"U0\" : "))
     U0=U
-    q=int(input("Valeur de la raison q : "))
+    q=int(input("Valeur de la raison \"q\" : "))
     L=int(input("Longueur de la suite : "))
     P=0
     print("U0=",U0)
@@ -28,5 +28,6 @@ if Suite==2:
         print("U",P,"=",U)
         print("Le pas est égale à",k)
         print()
+print()
 print()
 print("Fin de la suite.")
